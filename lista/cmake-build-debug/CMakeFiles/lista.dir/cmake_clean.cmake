d2834d58fb53190385297a7840b63012423357a9
Jeferson <jeferson.hsa@gmail.com>
2018-03-08 19:21:16 -0300

    reorganização

---
+++ lista/cmake-build-debug/CMakeFiles/lista.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lista.dir/main.c.o"
+  "lista.pdb"
+  "lista"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lista.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
